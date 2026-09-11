2afb28215348ab08d7499431183234a1cb538f84
PingWinCheck <ankolt@yandex.ru>
2026-09-11 21:18:05 +0300

    nullable reviewed columns

--- alembic/env.py
+++ alembic/env.py
@@ -7,6 +7,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 from core import Base, conf
+from shop import models
 from authorization.models import User
 
 # this is the Alembic Config object, which provides
