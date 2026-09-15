417f082e37c0838706fb946e8a48ee0f10ba116d
PingWinCheck <ankolt@yandex.ru>
2026-09-15 20:03:56 +0300

    refactors schemas, add ApplicationResponseSchema

--- shop/schemas.py
+++ shop/schemas.py
@@ -1,13 +1,16 @@
+from datetime import datetime
 from uuid import UUID
 
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 from shop.models import ApplicationStatus
 
-
-class ShopSchema(BaseModel):
+class ShopBaseSchema(BaseModel):
     title: str
     description: str
+
+
+class ShopCrateSchema(ShopBaseSchema):
     address: "AddressSchema"
 
 class AddressSchema(BaseModel):
@@ -15,12 +18,30 @@ class AddressSchema(BaseModel):
     street: str
     home: str
 
-class ResponseCreateShopApplication(BaseModel):
-    title: str
-    description: str
+class ResponseCreateShopApplicationSchema(ShopBaseSchema):
     status: str
 
 class ChangeApplication(BaseModel):
     id: int
     status: ApplicationStatus
     reason: str | None = None
+
+class ShopSchema(ShopBaseSchema):
+    model_config = ConfigDict(from_attributes=True)
+    id: int
+    created_at: datetime
+    updated_at: datetime
+    owner_id: UUID
+    address_id: int
+    status: ApplicationStatus
+    reviewed_at: datetime | None = None
+    reviewed_by_id: UUID | None = None
+    reason: str | None = None
+
+class ShopHistorySchema(ShopSchema):
+    shop_id: int
+
+class ApplicationResponseSchema(BaseModel):
+    shop: ShopSchema
+    shop_history: list[ShopHistorySchema] | None = None
+
